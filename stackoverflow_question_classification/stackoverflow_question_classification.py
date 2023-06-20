b13c2d12ac5afbb29210054bb696f5bbe31439d1
Nikolina Kokor <nkokor@icloud.com>
2023-06-21 00:48:54 +0200

    stackoverflow question classification

--- stackoverflow_question_classification/stackoverflow_question_classification.py
+++ stackoverflow_question_classification/stackoverflow_question_classification.py
@@ -6,41 +6,32 @@ from keras import models
 from keras import layers
 from keras.preprocessing.text import Tokenizer
 
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
 from matplotlib import pyplot as plt
 
-
 #characteristics data vectorization function
-def vectorize_sequences(sequences, dimension=4000):
+def vectorize_sequences(sequences, dimension=500):
   results = np.zeros((len(sequences), dimension))
   for i, sequence in enumerate(sequences):
-    results[i, sequence] = 1
-  return results
-
-#labels vectorization function
-def vectorize_labels(labels):
-  results = np.zeros(len(labels))
-  for i, label in enumerate(labels):
-    if(label.lower() == 'spam'):
-      results[i] = 1
-    elif(label.lower() == 'ham'):
-      results[i] = 0
+    results[i, sequence] = 1.
   return results
 
 #model defining function
 def define_model():
   model = models.Sequential()
-  model.add(layers.Dense(8, activation='relu', input_shape=(4000,)))
+  model.add(layers.Dense(32, activation='relu', input_shape=(500,)))
   model.add(layers.Dense(8, activation='relu'))
-  model.add(layers.Dense(1, activation='sigmoid'))
-  model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+  model.add(layers.Dense(4, activation='softmax'))
+  model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
   return model
 
 
 #loading data from an external file
 current_directory = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_directory, 'data', 'SpamDetectionData.txt')
+file_path = os.path.join(current_directory, 'data', 'stackoverflow.csv')
 data = pd.read_csv(file_path, sep=',')
 
 #inspecting data structure by viewing a few rows
@@ -50,46 +41,41 @@ print(data.head(5))
 print(data.describe())
 
 #splitting data to characteristics data and labels
-x = data['Message']
-y = data['Label']
+x = data['post']
+y = data['tags']
 
-#removing <p> and </p> tags from data
-for i in range(0, len(x)):
-  x[i] = x[i].strip('<p>')
-  x[i] = x[i].strip('</p>')
+#viewing possible labels
+print(set(y))
+
+#label one-hot-encoding
+le = LabelEncoder()
+y = le.fit_transform(y)
+y = to_categorical(y)
 
 #splitting data to train and test data with a 10% test size
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
-#characteristics data tokenization
-tokenizer = Tokenizer()
+#characterstics data tokenization
+tokenizer = Tokenizer(num_words=500)
 tokenizer.fit_on_texts(x_train)
 train_data_seq = tokenizer.texts_to_sequences(x_train)
 test_data_seq = tokenizer.texts_to_sequences(x_test)
 
-#viewing created index and word counts
-print(tokenizer.word_index)
-print(tokenizer.word_counts)
-
-#data vectorization
+#characteristics data vectorization
 train_data_vec = vectorize_sequences(train_data_seq)
 test_data_vec = vectorize_sequences(test_data_seq)
-train_label_vec = vectorize_labels(y_train)
-test_label_vec = vectorize_labels(y_test)
 
-#model defining and training with 30% validation split
+#model defining and viewing
 model = define_model()
-history = model.fit(train_data_vec, train_label_vec, epochs=5, batch_size=128, validation_split=0.3)
+print(model.summary)
 
-#model evaluation
-results = model.evaluate(test_data_vec, test_label_vec)
-print('Loss: ', results[0])
-print('Accuracy: ', results[1] * 100, '%')
+#model training with 25% validation split
+history = model.fit(train_data_vec, y_train, epochs=8, batch_size=8, validation_split=0.25)
 
 #accuracy graph plotting
 acc = history.history['accuracy']
 val_accuracy = history.history['val_accuracy']
-epochs = range(1, 6)
+epochs = range(1, len(acc) + 1)
 plt.plot(epochs, acc, 'bo', label='accuracy')
 plt.plot(epochs, val_accuracy, 'b', label='validation accuracy')
 plt.title('Accuracy graph')
@@ -101,6 +87,7 @@ plt.show()
 #loss graph plotting
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
 plt.plot(epochs, loss, 'bo', label='loss')
 plt.plot(epochs, val_loss, 'b', label='validation loss')
 plt.title('Loss graph')
@@ -109,9 +96,7 @@ plt.ylabel('loss')
 plt.legend()
 plt.show()
 
-#using the model on unseen data
-message = 'Dear students, your final grades will be uploaded today. I hope you all have a great summer. See you in september!'
-message_seq = tokenizer.texts_to_sequences([message])
-message_vec = vectorize_sequences(message_seq)
-prediction = model.predict(message_vec)
-print(prediction)
+#model evaluation
+results = model.evaluate(test_data_vec, y_test)
+print('Loss: ', results[0])
+print('Accuracy: ', results[1] * 100, '%')
